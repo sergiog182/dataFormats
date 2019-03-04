47def3af5e1c49cb012e1ffd2a7a20d584e57c45
Sergio Gutierrez <sergioa.gutierrezs@gmail.com>
2019-03-04 17:36:59 -0500

    Primer commit, proyecto camel con manejo de dataformats en jaxb

---
+++ src/main/java/com/beans/AcceptorBean.java
@@ -0,0 +1,30 @@
+package com.beans;
+
+import com.dataformats.*;
+
+public class AcceptorBean {
+	public void accept(Order order) {
+		int totalOrder = 0;
+		int totalItems = 0;
+		for (Item item : order.getItems().getItem()) {
+			int itemValue = Integer.parseInt(item.getValue());
+			int itemQuantity = Integer.parseInt(item.getQuantity());
+			
+			totalOrder = totalOrder + itemValue;
+			totalItems = totalItems + itemQuantity;
+		}
+		order.setItemsQuantity(Integer.toString(totalItems));
+		
+		int total = Integer.parseInt(order.getTotal());
+		if (total != totalOrder) {
+			order.setTotal(Integer.toString(totalOrder));
+			order.setComments("Valor ajustado: " + total);
+		}
+		
+		if (totalOrder >= 1000000) {
+			order.setAccepted("true");
+		} else {
+			order.setAccepted("false");
+		}
+	}
+}
---
+++ src/main/java/com/dataformats/Items.java
@@ -0,0 +1,78 @@
+package com.dataformats;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="item" maxOccurs="unbounded" minOccurs="0">
+ *           &lt;complexType>
+ *             &lt;complexContent>
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                 &lt;sequence>
+ *                   &lt;element name="itemName" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *                   &lt;element name="quantity" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *                   &lt;element name="value" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *                 &lt;/sequence>
+ *               &lt;/restriction>
+ *             &lt;/complexContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "item"
+})
+public class Items {
+
+    protected List<Item> item;
+
+    /**
+     * Gets the value of the item property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the item property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getItem().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Order.Items.Item }
+     * 
+     * 
+     */
+    public List<Item> getItem() {
+        if (item == null) {
+            item = new ArrayList<Item>();
+        }
+        return this.item;
+    }
+
+}
